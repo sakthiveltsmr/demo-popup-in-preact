8a12957a52f853eb60a18922969b0cbbd1600bd5
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-09-29 15:51:54 +0530

    fix:cookies

--- src/components/app.js
+++ src/components/app.js
@@ -10,20 +10,45 @@ import Profile from '../routes/profile';
 
 import { useState } from 'preact/hooks';
 
+import Model from "./modelpopup"
 
 const App = () => {
 const [popups,setpopup]=useState(false)
-	
-window.onmouseout=(event)=>{if(event.screenY<200) {
-	setpopup(true)
 
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
 }
-}
+const storageType = cookieStorage;
+const consentPropertyName = 'popups';
+const shouldShowPopup = () => storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+	
+  window.onmouseout=(event)=>{
+	if(event.screenY<115) {
+	
+          const value =shouldShowPopup()
+		  if(!value){
+			setpopup(true)
+			saveToStorage(storageType);
+		  }
+
+   }
+ }
 		
 	  
 	
 	return(<div id="app" >
-		
+		{popups && <Model setpopup={setpopup} />}
 		<Header />
 		
 		<Router>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,19 +1,14 @@
 import { h } from 'preact';
 import style from './style.css';
 
-import Model from '../../components/modelpopup';
-const Home = ({pop,setpopup}) => {
-	
-	
-
-	
-	
+// import Model from '../../components/modelpopup';
+const Home = ({setpopup}) => {
 	
 	return(
 	<div class={style.home}>
 		<h1 onClick={()=>setpopup(true)}>Home</h1>
 		<p>This is the Home component.</p>
-	    {pop &&	<Model  onset={setpopup} />}
+	   
 	</div>
 	)
 };
